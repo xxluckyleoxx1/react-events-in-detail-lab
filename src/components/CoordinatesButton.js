73d767490518fe1566d36cfca73cd7f8d2c98bf8
Leonides <xxluckyleoxx1@gmail.com>
2021-04-28 15:00:12 -0400

    errors

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,26 @@
 // Code CoordinatesButton Component Here
+import React, { Component} from 'react';
+
+class CoordinatesButton extends Component {
+    
+    
+    coordinates = (event) => {
+        // let screenLog = document.querySelector("")
+        event.persist();
+        let posX = event.clientX;
+        let posY = event.clientY;
+        let posArray = [posX, posY]
+        return posArray;
+    }
+
+    render(){
+        return(
+            <button onClick= {this.props.onReceiveCoordinates(this.coordinates())}></button>
+        )
+    }
+
+
+
+}
+
+export default CoordinatesButton; 
